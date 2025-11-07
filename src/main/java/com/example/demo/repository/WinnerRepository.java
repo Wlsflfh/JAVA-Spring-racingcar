f7ed62b72a68a5060b8427dacf3e260a77adbf2e
Jinriro <riro12.hanyang.ac.kr>
2025-11-07 23:39:19 +0900

    feat: 우승자 저장한 레포지토리 구현

--- src/main/java/com/example/demo/repository/WinnerRepository.java
+++ src/main/java/com/example/demo/repository/WinnerRepository.java
@@ -1,2 +1,7 @@
-package com.example.demo.repository;public class WinnerRepository {
+package com.example.demo.repository;
+
+import com.example.demo.domain.Winners;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WinnerRepository extends JpaRepository<Winners, Long> {
 }
