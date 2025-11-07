0976675a1c9c02e554e7b234420974b579ee1fc9
Jinriro <riro12.hanyang.ac.kr>
2025-11-07 23:41:19 +0900

    refactor: 네이밍 수정

--- src/main/java/com/example/demo/repository/SpringDataJpaWinnerRepository.java
+++ src/main/java/com/example/demo/repository/SpringDataJpaWinnerRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.domain.Winners;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WinnerRepository extends JpaRepository<Winners, Long> {
+public interface SpringDataJpaWinnerRepository extends JpaRepository<Winners, Long> {
 }
