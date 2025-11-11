86ca46da0a8afca64ca352dfeed5ee3b5f378c69
Jinriro <riro12.hanyang.ac.kr>
2025-11-11 13:49:33 +0900

    test: Cars test 코드 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class CarsTest {
-  
+
+    @Test
+    @DisplayName("중복된 자동차 이름들 텍스트에 대하여 예외를 발생시키는지 확인한다.")
+    void validateDuplicateTest() {
+        // given
+        String duplicateCarNames = "tesla,tesla,benz";
+
+        // when - then
+        assertThatThrownBy(() -> new Cars(duplicateCarNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
 }
